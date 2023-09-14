3bcd704f816dbda7e9f0f9ab3449919eb01b7ce2
Reuben Ramos <rsramos23@gmail.com>
2023-09-14 14:34:14 -0700

    deleted comments in index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// let num = prompt('Guess a number between 1 and 10:');
-// if (num == 5) {
-//     ('You guessed the right number!');
-// } else {
-// ('You guessed the wrong number!');
-// }
-// function num() {
-//     if (num==5){
-//     alert('you got it right')
-// }else{
-// alert('you got it wrong')
-// }
-// }
-
 function game(){
  const num = prompt('Guess a number between 1 and 10:');
 if (num == 5){
