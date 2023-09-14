ebffa2735fb635beca1d20d67fb2137c7d03c3f9
Reuben Ramos <rsramos23@gmail.com>
2023-09-14 11:44:22 -0700

    added button

---
+++ index.js
@@ -0,0 +1,10 @@
+// let num = prompt('Guess a number between 1 and 10:');
+// if (num == 5) {
+//     ('You guessed the right number!');
+// } else {
+// ('You guessed the wrong number!');
+// }
+function num() {
+    alert('pick a number from 1-10')
+}
+
