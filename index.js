67dee9d6d28ffdeb85faba8c5c79428683c386f2
Reuben Ramos <rsramos23@gmail.com>
2023-09-14 14:33:34 -0700

    added Function game to index.js

--- index.js
+++ index.js
@@ -4,7 +4,21 @@
 // } else {
 // ('You guessed the wrong number!');
 // }
-function num() {
-    alert('pick a number from 1-10')
+// function num() {
+//     if (num==5){
+//     alert('you got it right')
+// }else{
+// alert('you got it wrong')
+// }
+// }
+
+function game(){
+ const num = prompt('Guess a number between 1 and 10:');
+if (num == 5){
+alert('You are correct')
+}else{
+    alert('Sorry try again')
+}
+alert('Thank you for playing')
 }
 
